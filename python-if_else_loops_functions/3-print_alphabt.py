2c08b3409f8a7b6c1ac24fa8eb0976e061f3c2b9
rrarima <5708@holbertonstudents.com>
2023-01-16 01:15:59 -0800

    task 3 attempt

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-print(''.join('{}'.format(chr(ch))
-            for ch in range(97, 123) if chr(ch) not in ['q', 'e']))
+print(''.join(
+        '{}'.format(chr(ch))
+        for ch in range(97, 123)
+        if chr(ch) not in ['q', 'e']))
