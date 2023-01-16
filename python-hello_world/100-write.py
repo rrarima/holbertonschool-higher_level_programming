f84c58955590ad3ba5106b497194c8747cd8b204
rrarima <5708@holbertonstudents.com>
2023-01-15 18:10:24 -0800

    task 10 attempt

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015\n-10-19")
-sys.stdout.write()
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015\n-10-19\n")
 sys.exit(1)
