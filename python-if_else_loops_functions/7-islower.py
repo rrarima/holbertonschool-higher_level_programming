cc5d722f98560325d59af2773b141ecacfc56d5b
rrarima <5708@holbertonstudents.com>
2023-01-17 01:14:14 -0800

    code refactor for ord

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == '':
-        return None
-    if c >= 'A' and c <= 'Z':
-        return False
-    return True
+   return(ord('a') <= ord(c) <= ord('z'))
