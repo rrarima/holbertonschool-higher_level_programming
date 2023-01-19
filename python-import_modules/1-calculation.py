9676cdc0b164a49f33e9c84f5107af2166133ef8
epsfrmthSE <rarimar4@gmail.com>
2023-01-19 19:58:08 +1100

    task 1 attempt

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(add(a,b))
+    print(sub(a,b))
+    print(mul(a,b))
+    print(div(a,b))
