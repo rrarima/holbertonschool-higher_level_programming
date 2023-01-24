b7c169eded4376f8d0de12f08cdb99b98709d698
rrarima <5708@holbertonstudents.com>
2023-01-24 20:15:44 +1100

    changed list array

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if  len(my_list) == 0:
         return None
-    largest = 0
-    for idx in my_list:
-        if idx > largest:
-            largest = idx
+    largest = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > largest:
+            largest = my_list[i]
     return largest
