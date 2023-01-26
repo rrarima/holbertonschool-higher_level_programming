0b11c2ea2fcb2c5f45e7c5194e5647e987631ced
rrarima <5708@holbertonstudents.com>
2023-01-26 11:48:09 +1100

    task 7 fix

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import copy
+
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
+    new_dict = cop.deepcopy(a_dictionary)
     new_dict[key] = value
     return new_dict
