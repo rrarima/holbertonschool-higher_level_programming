abd852bf7b83320f6f6495afc41061eab3f4f45c
rrarima <5708@holbertonstudents.com>
2023-01-27 10:34:54 +1100

    forgot to use map

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = [i * number for i in my_list]
-
-    return new_list
+    return list(map(lambda i: i * number, my_list))
