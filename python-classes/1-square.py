aa738c85695c6229ddadbf444fdb1996f20c3af4
rrarima <5708@holbertonstudents.com>
2023-01-30 12:13:05 +1100

    create class square and set size as private instace atrtibute

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Square class taking size as a private attribute
+"""
+
+
+class Square:
+    """
+    making size a private instance
+    """
+    def __init__(self, size):
+        self.__size = size
