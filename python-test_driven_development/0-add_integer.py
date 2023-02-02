3bf974b3af8db9f1c47b568cacfa117c808826f7
rrarima <5708@holbertonstudents.com>
2023-02-02 17:14:26 +1100

    add 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,13 +5,10 @@
 def add_integer(a, b=98):
     """Adding edge cases"""
 
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
-        raise TypeError("a must be an integert")
-    elif type(b) != int:
-        raise TypeError("b must be an integert")
-    else:
-        return a + b
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
