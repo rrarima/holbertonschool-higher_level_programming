6b6df3baafd59336f71c9c3b2fd340e07e9e970f
rrarima <5708@holbertonstudents.com>
2023-02-03 12:51:26 +1100

    define rectange task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Rectangle Class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initialise Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get rectangle width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get rectangle height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Set rectangle width"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise TypeError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Set rectangle height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise TypeError('height must be >= 0')
+        else:
+            self.__height = value
