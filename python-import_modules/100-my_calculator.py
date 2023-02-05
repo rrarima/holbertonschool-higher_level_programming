5a39abaa24c063f32fe104c40c9e42ab006cacd3
rrarima <5708@holbertonstudents.com>
2023-02-05 22:04:00 +1100

    calculator that takes in args

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    unkown_op = "Unknown operator. Available operators: +, -, * and /"
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a, operator, b = sys.argv[1:]
+    a = int(a)
+    b = int(b)
+
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print(unkown_op)
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+    sys.exit(0)
