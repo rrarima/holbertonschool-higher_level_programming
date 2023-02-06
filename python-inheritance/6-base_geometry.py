55796d526d4b29c054b27f389d9060aa56a5b960
rrarima <5708@holbertonstudents.com>
2023-02-06 21:57:10 +1100

    add area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
