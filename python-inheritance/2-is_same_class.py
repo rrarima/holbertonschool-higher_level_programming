1c4d28e2e5707e1a3fef44720bd1699be44bb04f
rrarima <5708@holbertonstudents.com>
2023-02-06 20:59:39 +1100

    return class of object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Return True is object is exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_function"""
+
+    return type(obj) is a_class
