8b99005ad7ae1fed670e0dbeb2d9f366a1ddcb81
rrarima <5708@holbertonstudents.com>
2023-02-06 21:59:56 +1100

    inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,4 @@
 
 def inherits_from(obj, a_class):
     """inherits_from function"""
-
-    if isinstance(obj, a_class) and not issubclass(type(obj), a_class):
-        return False
-    else:
-        return True
+        return(issubclass(type(obj), a_class) and type(obj) != a_class)
