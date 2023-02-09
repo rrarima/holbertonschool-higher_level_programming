850897993aae882e923de86d8a581ead5611f35e
rrarima <5708@holbertonstudents.com>
2023-02-09 17:37:12 +1100

    return area of rectangle and string reppresentation

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Inherit BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle inherits BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialise rectangle"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string area of rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
