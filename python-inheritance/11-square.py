5b06b1d3166d241f515210f4d8139a749c5da0ff
rrarima <5708@holbertonstudents.com>
2023-02-09 20:15:19 +1100

    return area of square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create Square class inheriting Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialise square"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
