ecdbcff8378d10a45b17ba6bb58f833a96d077e4
rrarima <5708@holbertonstudents.com>
2023-02-10 17:47:50 +1100

    creates an Object from JSON file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """Create object from JSON file"""
     def load_from_json_file(filename):
-        with open(filename, "r", encoding="utf-8") as f:
-            return json.loads(f)
+        with open(filename) as f:
+            return json.load(f)
