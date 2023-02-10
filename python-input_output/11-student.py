d31bb73e313a0067c8c792bafb11bba53c57803c
rrarima <5708@holbertonstudents.com>
2023-02-10 20:20:04 +1100

    return key and value dict

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class that defines student"""
+
+
+class Student:
+    """Student class initialise"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary string"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
