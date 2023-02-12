bdd4a3567246537c7796cfdda6b8f707b4fd3a7c
rrarima <5708@holbertonstudents.com>
2023-02-12 11:23:25 +1100

    return the squared of each number in a matrix

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = [list(map(lambda x: x **2, row)) for row in matrix]
-    return new_matrix
+    return list(map(lambda row: list(map(lambda x: x **2, row)), matrix))
