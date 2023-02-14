ce45911e2c27443b7426cc7065cc95d9b771b899
rrarima <5708@holbertonstudents.com>
2023-02-14 11:28:31 +1100

    adding private classes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -57,7 +57,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if type(value) != int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
