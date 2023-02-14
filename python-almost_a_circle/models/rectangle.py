e1e8781cdca8c6cd5a113c0614bd9d4f607c0611
rrarima <5708@holbertonstudents.com>
2023-02-14 11:09:44 +1100

    adding private classes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(Base):
 
         @height.setter
         def height(self, value):
-            self.__width = value
+            self.__height = value
 
         @property
         def x(self):
