7deffa8088081d569a460f8bfc5febeaa5baa19e
rrarima <5708@holbertonstudents.com>
2023-02-16 16:38:48 +1100

    display for x axis

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,11 +76,9 @@ class Rectangle(Base):
 
     def display(self):
         """Display rectangle"""
-        if self.__y != 0:
-            for newline in range(self.__y):
-                print()
+        print("\n" * self.y, end="")
         for row in range(self.__height):
-            print((self.__x * "") + (self.__width * '#'))
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return string format"""
