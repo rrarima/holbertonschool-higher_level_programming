391692ee06d30aa3150895e89eaa7d52abfb0c0a
rrarima <5708@holbertonstudents.com>
2023-02-16 19:28:37 +1100

    returning JSON for base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base Class"""
-
+import json
 
 class Base:
     """Base class init"""
@@ -15,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return JSON string format"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
