41b81b8e42d3dbc9e283a972ca9feac341b1448a
rrarima <5708@holbertonstudents.com>
2023-02-16 19:01:00 +1100

    returning dict for rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,4 +104,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Return dict format"""
-        return self.__dict__
+        return {'id':self.id, 'width':self.width, 'height':self.height,
+                'x':self.x, 'y':self.y}
