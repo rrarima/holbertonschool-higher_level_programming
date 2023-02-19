de1f295fb7e94fe9cd8c170d1889f81a9bcc809f
rrarima <5708@holbertonstudents.com>
2023-02-19 13:53:32 +1100

    test case

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -14,17 +14,10 @@ class TestBase(TestCase):
 
     def test_id(self):
         """Test base id if it exist"""
-        self.b1 = Base()
-        self.b2 = Base()
-        self.b3 = Base()
-        self.b4 = Base(5)
-        self.b5 = Base(-1)
-
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 2)
-        self.assertEqual(self.b3.id, 3)
-        self.assertEqual(self.b4.id, 5)
-        self.assertEqual(self.b5.id, -1)
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base(None).id, 2)
+        self.assertEqual(Base(20).id, 20)
+        self.assertEqual(Base(-20).id, -20)
 
 if __name__ == '__main__':
     unittest.main()
