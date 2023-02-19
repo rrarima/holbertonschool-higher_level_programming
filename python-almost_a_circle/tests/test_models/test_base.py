0a794655c41f9715a487f62f1d7381b387c24a05
rrarima <5708@holbertonstudents.com>
2023-02-19 13:16:22 +1100

    dir fix

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unit test for base class"""
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """Base class unit test"""
+
+    def test_id(self):
+        """Test base id if it exist"""
+        self.b1 = Base()
+        self.assertEqual(self.b1.id, 1)
+        self.b2 = Base()
+        self.assertEqual(self.b2.id, 2)
+        self.b3 = Base()
+        self.assertEqual(self.b3.id, 3)
+        self.b4 = Base(5)
+        self.assertEqual(self.b4.id, 5)
