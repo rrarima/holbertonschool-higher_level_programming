d45bfe92015641945f566b718d5e23a5c026cb7c
rrarima <5708@holbertonstudents.com>
2023-02-19 14:50:18 +1100

    test case

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,24 +1,49 @@
 #!/usr/bin/python3
 """Rectangle Unit test"""
-from unittest import TestCase
+import unittest
 from models.rectangle import Rectangle
-from models.base import Base
 
 
-class TestRectangle(TestCase):
+class TestRectangle(unittest.TestCase):
     """Rectangle unit test"""
 
-    def setup(self):
-        """Grabs obj count"""
-        Base._Base__nb_objects = 0
-        self.assertEqual(Base._Base__nb_objects, 0)
+    def test_create(self):
+        """
+        Test if creating a new Rectangle works.
+        """
+        r = Rectangle(1, 2)
+        self.assertTrue(r)
 
-    def test_area(self):
-        t1 = Rectangle(1, 2)
-        t2 = Rectangle(1, 2, 3, 4, 5)
-
-        self.assertEqual(t1.area(), 2)
-        self.assertEqual(t1.area(), 2)
+    def test_create_args(self):
+        """
+        Test if creating a new Rectangle with various arguments works.
+        """
+        r1 = Rectangle(1, 2, 3)
+        r2 = Rectangle(1, 2, 3, 4)
+        r3 = Rectangle("1", 2)
+        r4 = Rectangle(1, "2")
+        r5 = Rectangle(1, 2, "3")
+        r6 = Rectangle(1, 2, 3, "4")
+        r7 = Rectangle(1, 2, 3, 4, 5)
+        r8 = Rectangle(-1, 2)
+        r9 = Rectangle(1, -2)
+        r10 = Rectangle(0, 2)
+        r11 = Rectangle(1, 0)
+        r12 = Rectangle(1, 2, -3)
+        r13 = Rectangle(1, 2, 3, -4)
+        self.assertTrue(r1)
+        self.assertTrue(r2)
+        self.assertTrue(r3)
+        self.assertTrue(r4)
+        self.assertTrue(r5)
+        self.assertTrue(r6)
+        self.assertTrue(r7)
+        self.assertTrue(r8)
+        self.assertTrue(r9)
+        self.assertTrue(r10)
+        self.assertTrue(r11)
+        self.assertTrue(r12)
+        self.assertTrue(r13)
 
 if __name__ == "__main__":
     unittest.main()
