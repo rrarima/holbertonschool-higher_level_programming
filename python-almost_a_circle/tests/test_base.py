e7dfad5af2974712e71acd219e073b6107bafd4b
rrarima <5708@holbertonstudents.com>
2023-02-19 12:57:02 +1100

    test base id

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,3 +10,9 @@ class TestBase(unittest.TestCase):
         """Test base id if it exist"""
         self.b1 = Base()
         self.asserEqual(self.b1.id, 1)
+        self.b2 = Base()
+        self.asserEqual(self.b2.id, 2)
+        self.b3 = Base()
+        self.asserEqual(self.b3.id, 3)
+        self.b4 = Base(5)
+        self.asserEqual(self.b4.id, 5)
