e77aaadaffe7117022ce134fc571a8ad773bfdf7
rrarima <5708@holbertonstudents.com>
2023-02-19 15:08:56 +1100

    test case

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,7 @@ class TestBase(unittest.TestCase):
     """
     Test cases for the Base class.
     """
-    
+
     def test_auto_assign_id(self):
         """
         Test that Base assigns a unique ID automatically.
@@ -13,7 +13,7 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertNotEqual(b1.id, b2.id)
-    
+
     def test_auto_assign_id_plus_one(self):
         """
         Test that Base assigns an ID that is one greater than the
@@ -24,26 +24,26 @@ class TestBase(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b1.id + 1, b2.id)
         self.assertEqual(b2.id + 1, b3.id)
-    
+
     def test_save_passed_id(self):
         """
         Test that Base saves the ID passed to it.
         """
         b = Base(89)
         self.assertEqual(b.id, 89)
-    
+
     def test_to_json_string_with_none(self):
         """
         Test that Base.to_json_string returns "[]" when given None.
         """
         self.assertEqual(Base.to_json_string(None), "[]")
-    
+
     def test_to_json_string_with_empty_list(self):
         """
         Test that Base.to_json_string returns "[]" when given an empty list.
         """
         self.assertEqual(Base.to_json_string([]), "[]")
-    
+
     def test_to_json_string_with_list_of_dict(self):
         """
         Test that Base.to_json_string returns a string when given a list
@@ -51,19 +51,19 @@ class TestBase(unittest.TestCase):
         """
         lst = [{'id': 12}]
         self.assertIsInstance(Base.to_json_string(lst), str)
-    
+
     def test_from_json_string_with_none(self):
         """
         Test that Base.from_json_string returns an empty list when given None.
         """
         self.assertEqual(Base.from_json_string(None), [])
-    
+
     def test_from_json_string_with_empty_list(self):
         """
         Test that Base.from_json_string returns an empty list when given "[]".
         """
         self.assertEqual(Base.from_json_string("[]"), [])
-    
+
     def test_from_json_string_with_list_of_dict(self):
         """
         Test that Base.from_json_string returns a list of dictionaries, each
@@ -74,6 +74,3 @@ class TestBase(unittest.TestCase):
         self.assertIsInstance(lst, list)
         self.assertIsInstance(lst[0], dict)
         self.assertIn('id', lst[0])
-
-if __name__ == '__main__':
-    unittest.main()
