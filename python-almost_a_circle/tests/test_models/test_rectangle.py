0a1fafba4c404a3b62283d8d241e3b70b712d6a4
rrarima <5708@holbertonstudents.com>
2023-02-19 14:59:28 +1100

    test case

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,8 @@
 """Rectangle Unit test"""
 import unittest
 from models.rectangle import Rectangle
+from models.square import Square
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
@@ -13,37 +15,6 @@ class TestRectangle(unittest.TestCase):
         """
         r = Rectangle(1, 2)
         self.assertTrue(r)
-
-    def test_create_args(self):
-        """
-        Test if creating a new Rectangle with various arguments works.
-        """
-        r1 = Rectangle(1, 2, 3)
-        r2 = Rectangle(1, 2, 3, 4)
-        r3 = Rectangle("1", 2)
-        r4 = Rectangle(1, "2")
-        r5 = Rectangle(1, 2, "3")
-        r6 = Rectangle(1, 2, 3, "4")
-        r7 = Rectangle(1, 2, 3, 4, 5)
-        r8 = Rectangle(-1, 2)
-        r9 = Rectangle(1, -2)
-        r10 = Rectangle(0, 2)
-        r11 = Rectangle(1, 0)
-        r12 = Rectangle(1, 2, -3)
-        r13 = Rectangle(1, 2, 3, -4)
-        self.assertTrue(r1)
-        self.assertTrue(r2)
-        self.assertTrue(r3)
-        self.assertTrue(r4)
-        self.assertTrue(r5)
-        self.assertTrue(r6)
-        self.assertTrue(r7)
-        self.assertTrue(r8)
-        self.assertTrue(r9)
-        self.assertTrue(r10)
-        self.assertTrue(r11)
-        self.assertTrue(r12)
-        self.assertTrue(r13)
 
 if __name__ == "__main__":
     unittest.main()
