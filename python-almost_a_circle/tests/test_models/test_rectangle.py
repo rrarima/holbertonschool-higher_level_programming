77518f0b125a7806b016595065b8d1abca16d842
rrarima <5708@holbertonstudents.com>
2023-02-19 15:05:43 +1100

    test case

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,12 +9,20 @@ from models.base import Base
 class TestRectangle(unittest.TestCase):
     """Rectangle unit test"""
 
-    def test_create(self):
-        """
-        Test if creating a new Rectangle works.
-        """
-        r = Rectangle(1, 2)
-        self.assertTrue(r)
+    def update(self):
+        """Tears down obj count"""
+        Base._Base__nb_objects = 0
+        self.assertEqual(Base._Base__nb_objects, 0)
+    
+    def test_area(self):
+        """Testing area()"""
+        o1 = Rectangle(1, 2)
+        o2 = Rectangle(1, 3, 0, 0, 12)
+        o3 = Rectangle(123, 123)
+
+        self.assertEqual(o1.area(), 2)
+        self.assertEqual(o2.area(), 3)
+        self.assertEqual(o3.area(), 15129)
 
 if __name__ == "__main__":
     unittest.main()
