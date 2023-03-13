afb38bc8c23e6ad486b593b169bf2d61f6c1b82d
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-13 20:04:50 +1100

    script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -2,8 +2,8 @@
 
 ALTER DATABASE
     hbtn_0c_0
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE
     first_table
