b1ea4918c34dd6e2587db1855c71b4e15515e213
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-15 21:22:14 +1100

    script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 
-SELECT NAME
+SELECT name
 FROM tv_show_genres
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
