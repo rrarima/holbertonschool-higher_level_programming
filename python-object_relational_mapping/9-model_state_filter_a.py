e3cd7709ba21e75f892925a3d0028e0f1092597e
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-19 10:19:49 +1100

    Lists all State objects that contain the letter a from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    # Get the connection arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create the connection engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            user, password, db_name),
+        pool_pre_ping=True)
+
+    # Create a configured Session class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    # Query the database and print the results
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id)
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+    # Close the session
+    session.close()
