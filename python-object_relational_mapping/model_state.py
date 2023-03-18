4cb35953aef50f06c72dcc9bf2135d544f773df0
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-18 23:16:35 +1100

    python file that contains the class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class."""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
