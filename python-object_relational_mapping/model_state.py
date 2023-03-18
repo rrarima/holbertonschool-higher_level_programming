6c73bb9c7ee0973866f976c81fab23a87996c174
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-18 23:18:09 +1100

    python file that contains the class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-python file that contains the class definition of a State and an instance 
+python file that contains the class definition of a State and an instance
 Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
