7b69e5a54e6e9b0c5f737c151cba65bf2c56a4ac
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-18 21:58:43 +1100

    script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -19,18 +19,15 @@ if __name__ == "__main__":
     # Create a cursor object to execute queries
     cursor = db.cursor()
 
-    # query = "SELECT cities.id, cities.name, states.name\
-    #          FROM cities\
-    #          WHERE cities.state_id = states.id\
-    #          ORDER BY cities.id"
+    query = "SELECT cities.id, cities.name, states.name\
+             FROM cities\
+             JOIN states ON cities.state_id = states.id\
+             ORDER BY cities.id"\
 
     #name = (sys.argv[4],)
 
     # Execute SQL query to fetch all the states in the database
-    cursor.execute("""SELECT cities.id, cities.name, states.name
-                      FROM cities
-                      WHERE cities.state_id = states.id
-                      ORDER BY cities.id""")
+    cursor.execute(query)
 
     # Fetch all the rows and display them
     rows = cursor.fetchall()
