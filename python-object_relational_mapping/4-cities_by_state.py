69402612cf5558aef8d2220f3d91cad30670179e
Eps (Rarima) <5708@holbertonstudents.com>
2023-03-18 20:13:09 +1100

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name\
+             FROM cities\
+             WHERE cities.state_id = states.id\
+             ORDER BY cities.id"
+             
+    name = (sys.argv[4],)
+    
+    # Execute SQL query to fetch all the states in the database
+    cursor.execute(query, name)
+
+    # Fetch all the rows and display them
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
