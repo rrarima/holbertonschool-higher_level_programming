32a0bde739e74193fc4eb763979334f8d7051ed9
rrarima <5708@holbertonstudents.com>
2023-04-10 16:22:35 +1000

    task 5 type

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const args = process.argv.slice(2);
 const num = parseInt(args[0]);
 
-if (isNaN(num)) {
-  console.log('Not a Number');
+if (isNaN(args[0])) {
+  console.log('Not a number');
 } else {
-  console.log(`My Number: ${num}`);
+  console.log(`My number: ${num}`);
 }
