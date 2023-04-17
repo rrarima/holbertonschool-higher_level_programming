267a5bf2899ad9d18ce7c10abd716203a3bf5107
rrarima <5708@holbertonstudents.com>
2023-04-17 15:28:44 +1000

    script that display the status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
+});
