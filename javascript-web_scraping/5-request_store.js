1fc90c5d38707e9989522ffe9b0a5c27425ae8fd
rrarima <5708@holbertonstudents.com>
2023-04-17 19:17:30 +1000

    script that gets the contents of a webpage and stores it in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (err, res, body) {
+  if (err) return console.log(err);
+  if (res.statusCode !== 200) return console.log(`code: ${res.statusCode}`);
+  fs.writeFile(filePath, body, 'utf-8', function (err) {
+    if (err) return console.log(err);
+  });
+});
