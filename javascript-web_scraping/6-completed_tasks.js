100aacce0ac2a08bed74e2c847fa051ca1df3c4c
rrarima <5708@holbertonstudents.com>
2023-04-22 13:47:14 +1000

    script that computes the number of tasks completed by user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) return console.error(error);
+  if (response.statusCode !== 200) return console.error(`Status code ${response.statusCode}`);
+
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = tasks.reduce((acc, task) => {
+    if (task.completed) {
+      const userId = task.userId;
+      acc[userId] = (acc[userId] || 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  console.log(completedTasksByUser);
+});
